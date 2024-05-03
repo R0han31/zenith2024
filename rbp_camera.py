a2f7a3160cbf05f0a4ba0a6f030c4d06413e02b1
Rohan Reddy <rohanyeluru31@gmail.com>
2024-05-03 22:45:35 +0530

    init picam

--- rbp_camera.py
+++ rbp_camera.py
@@ -3,11 +3,16 @@ import os
 try:
         import libcamera
         from picamera2 import Picamera2, Preview
+except RuntimeError:
+        pass
+
+try:
         picam = Picamera2()
 
         config = picam.create_preview_configuration(main={"size":(1600,1200)})
         config["transform"] = libcamera.Transform(hflip=1,vflip=1)
         picam.configure(config)
+
 except RuntimeError:
         pass
 
