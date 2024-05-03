38fc2b9367a7976d5dcc069e0fc9e36292eeff8c
Rohan Reddy <rohanyeluru31@gmail.com>
2024-05-03 22:52:59 +0530

    debug

--- rbp_camera.py
+++ rbp_camera.py
@@ -12,21 +12,19 @@ except RuntimeError:
 
 try:
         print("before init picam")
-        picam = Picamera2()
-        print("after init picam")
+        with Picamera2() as picam:
+                print("after init picam")
 
-        print("before config picam")
+                print("before config picam")
 
-        config = picam.create_preview_configuration(main={"size":(1600,1200)})
-        config["transform"] = libcamera.Transform(hflip=1,vflip=1)
-        picam.configure(config)
-        print("after config picam")
+                config = picam.create_preview_configuration(main={"size":(1600,1200)})
+                config["transform"] = libcamera.Transform(hflip=1,vflip=1)
+                picam.configure(config)
+                print("after config picam")
         
 except RuntimeError:
         pass
-except ImportError:
-        import libcamera
-        from picamera2 import Picamera2, Preview
+
 
 
 # picam.start_preview(Preview.QTGL)
